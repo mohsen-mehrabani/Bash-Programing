4a1e1430e7422891b6906728640f89adff969026
asus <mohsen.mehrabani@hotmail.com>
2020-04-13 23:55:33 +0430

    7:25 PM Monday, Farvardin 25, 1399 AP Coordinated Universal Time (UTC)

---
+++ Let.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+let "y=((x=20, 10/2))"
+
+echo $y
+# I don't why !!!!!
---
+++ forLoop.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+colors="red white red"
+
+for col in $colors 
+do
+       	echo $col
+done
+
+
+for col in "$colors"
+do
+	echo "$colors"
+done
+
+echo "$colors"
+echo $colors
