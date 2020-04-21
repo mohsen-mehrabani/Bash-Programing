413a8089f0cf648ee1aa10b194b459da550082ef
mohsen-mehrabani <mohsen.mehrabani@hotmail.com>
2020-04-21 09:38:42 +0430

    5:08 AM Tuesday, Ordibehesht 2, 1399 AP Coordinated Universal Time (UTC)

--- Projects/ConditionalStatements/CatFiles.sh
+++ Projects/ConditionalStatements/CatFiles.sh
@@ -12,6 +12,7 @@ E_Size=89
 if [[ -z "$1" && -z "$2" ]]
 then
   echo "No arguments given"
+  sleep 60
   exit $E_No_Args
 fi
 
@@ -19,24 +20,29 @@ if [[ $# -ne "$No_of_Args" ]]
 then
   # shellcheck disable=SC2006
   echo "USAGE:`basename "$0"` File1 File2"
+  sleep 60
   exit $E_BadARGS
 fi
 
 if [[ ! -e "$1" || ! -e "$2" ]]
 then
   echo "File does not exists"
+  sleep 60
   exit $E_NoFile
 elif [[ ! -f "$1" || ! -f "$2" ]]
   then
     echo "File need to be regular files"
+    sleep 60
     exit $E_NotFile
 elif [[ ! -r "$1" || ! -r "$2" ]]
   then
     echo "No read permission"
+    sleep 60
     exit $E_Unreadable
 elif [[ ! -s "$1" || ! -s "$2" ]]
   then
     echo "Files are zero-size"
+    sleep 60
     exit $E_Size
 fi
 cat "$1" "$2" | sort>file.txt
@@ -46,6 +52,9 @@ if [ $? -eq 0 ]
  then
    echo "Execution of the script was successful"
    cat file.txt
+   sleep 60
   else
     echo "Execution of the script failed"
+    sleep 60
 fi
+
