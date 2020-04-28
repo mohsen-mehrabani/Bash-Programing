3bf7c495dc1c1ef0e901f1462df752c0319285c3
mohsen-mehrabani <mohsen.mehrabani@hotmail.com>
2020-04-28 23:59:41 +0430

    7:59 PM Monday, Ordibehesht 10, 1399 AP Coordinated Universal Time (UTC)

---
+++ Projects/InternalCommand/Read.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+up=$'\x1b[A'
+down=$'\x[B'
+left=$'\x[D'
+right=$'\x[C'
+
+read -s -n3 -p "Press an arrow key: " arrow
+
+case "$arrow" in
+$up) echo "UP";;
+$down) echo "Down";;
+$
+$
+  
+  ;;
+esac
