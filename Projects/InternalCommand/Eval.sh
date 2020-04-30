112373cdd7da28f30c521a8355162f8533b500e1
mohsen-mehrabani <mohsen-mehrabani@hotmail.com>
2020-04-30 23:30:34 +0430

    6:58 PM Thursday, Ordibehesht 11, 1399 AP Coordinated Universal Time (UTC)

--- Projects/InternalCommand/Eval.sh
+++ Projects/InternalCommand/Eval.sh
@@ -2,7 +2,7 @@
 
 # shellcheck disable=SC2157
 # shellcheck disable=SC2236
-if [ ! -z S1 ] # -z The -z and -n operators return successfully if their arguments are empty or nonempty
+if [ ! -z S1 ] # The -z and -n operators return successfully if their arguments are empty or nonempty
 then
   process="ps -ef | grep $1"
 fi
---
+++ Projects/RegularExpressions/grep.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+E_NoPattern=71
+Dict=/home/asus/PycharmProjects/PythonPrograms/Bash-Programming/Projects/RegularExpressions/english-words-master/words.txt
+
+if [ -z "$1" ]; then
+  echo
+  echo "Usage: "
+  # shellcheck disable=SC2006
+  echo "`basename $0` \"Pattern,\""
+  echo "where \"Pattern\" is in the form"
+  echo "ooo..oo..o..."
+  echo
+  echo "The O's are the you already know,"
+  echo "and the periods are missing letters."
+  echo "Letters and periods can be in any position"
+  echo "for example: w...i....n"
+  echo
+  exit $E_NoPattern
+fi
+
+grep ^"$1"$ "$Dict"
