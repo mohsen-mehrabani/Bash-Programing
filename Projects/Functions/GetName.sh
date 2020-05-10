db8d1128a4f095a7069cf57ad76b42e85ee14518
mohsen-mehrabani <mohsen.mehrabani@hotmail.com>
2020-05-10 13:30:45 +0430

    9:00 AM Sunday, Ordibehesht 21, 1399 AP Coordinated Universal Time (UTC)

---
+++ Projects/Functions/GetName.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+Args=1
+E_BadArgs=85
+
+FILE=/etc/passwd
+pattern=$1
+
+if [ $# -ne "$Args" ]
+then
+  echo"Usage: `basename $0` USERNAME"
+  exit $E_BadArgs
+fi
+get_real_name()
+{
+  while read line
+  do
+    echo "$line" | grep $1 awk -F":" '{ print $5 }'
+  done
+}<$FILE
+
+get_real_name $pattern
---
+++ Projects/Functions/compare.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+E_Param_Err=250
+Equal=251
+
+function_max()
+{
+  if [ -z "$2" ]
+  then
+    return $E_Param_Err
+  fi
+  if [ "$1" -eq "$2" ]
+  then
+    return $Equal
+   else
+     if [ "$1" -gt "$2" ]
+     then
+       return $1
+       else
+         return $2
+     fi
+   fi
+}
+
+function_max 12 25
+
+Return_Value=$?
+if [ "$Return_Value" -eq "$E_Param_Err" ]
+then
+  echo "Function needs two parameters"
+  elif [ "$Return_Value" -eq "$Equal" ]
+  then
+    echo " Numbers are equal"
+    else
+      echo "Max number is $Return_Value"
+fi
+sleep 2
